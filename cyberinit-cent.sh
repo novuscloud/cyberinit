796a0711ab65663564a2d660b0f71845a3b28f49
novuscloud <novuscloud@gmail.com>
2020-05-03 18:58:07 +0800

    update

---
+++ cyberinit-cent.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum install screen -y
+wget https://raw.githubusercontent.com/novuscloud/cyberpanel/stable/cyberpanel.sh
+# sudo nohup sh <(curl cyberpanel.sh) -v ols -p deathgod305
+#sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh) <<< $'1\n1\ny\n\ns\ndeathgod305\ndeathgod305\nn\nn\nn\nn\n'
+chmod +x cyberpanel.sh
+./cyberpanel.sh <<< $'1\n1\ny\n\ns\ndeathgod305\ndeathgod305\nn\nn\nn\nn\n'
+yum install git -y
+git clone https://github.com/novuscloud/cybercp
+mv /cybercp/index.html /usr/local/CyberCP/index.html
+mv /cybercp/login.html /usr/local/CyberCP/loginSystem/templates/loginSystem/login.html
+mv /cybercp/fileManager.css /usr/local/CyberCP/public/static/filemanager/css/fileManager.css
+mv /cybercp/default.css /usr/local/CyberCP/public/static/baseTemplate/assets/themes/admin/color-schemes/default.css
+rm -rf cybercp
+rm -f /cyberpanel.sh 
+rm -f /etc/profile.d/cyberpanel.sh
+rm -rf cyberpanel
+rm -f cyberinit.sh
+wget https://raw.githubusercontent.com/novuscloud/cybercp-mycnf/master/my.cnf
+mv my.cnf /etc/my.cnf
+cyberpanel createPackage --owner admin --packageName standard --diskSpace 10000 --bandwidth 0 --emailAccounts 1 --dataBases 1 --ftpAccounts 1 --allowedDomains 1
+cyberpanel createPackage --owner admin --packageName professional --diskSpace 15000 --bandwidth 0 --emailAccounts 10 --dataBases 10 --ftpAccounts 10 --allowedDomains 5
+cyberpanel createPackage --owner admin --packageName premium --diskSpace 25000 --bandwidth 0 --emailAccounts 0 --dataBases 0 --ftpAccounts 0 --allowedDomains 0
+reboot
--- cyberinit-ubun.sh
+++ cyberinit-ubun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-yum install screen -y
 wget https://raw.githubusercontent.com/novuscloud/cyberpanel/stable/cyberpanel.sh
 # sudo nohup sh <(curl cyberpanel.sh) -v ols -p deathgod305
 #sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh) <<< $'1\n1\ny\n\ns\ndeathgod305\ndeathgod305\nn\nn\nn\nn\n'
 chmod +x cyberpanel.sh
 ./cyberpanel.sh <<< $'1\n1\ny\n\ns\ndeathgod305\ndeathgod305\nn\nn\nn\nn\n'
+apt-get install git -y
 git clone https://github.com/novuscloud/cybercp
 mv /cybercp/index.html /usr/local/CyberCP/index.html
 mv /cybercp/login.html /usr/local/CyberCP/loginSystem/templates/loginSystem/login.html
@@ -15,8 +15,8 @@ rm -f /cyberpanel.sh
 rm -f /etc/profile.d/cyberpanel.sh
 rm -rf cyberpanel
 rm -f cyberinit.sh
-wget https://raw.githubusercontent.com/novuscloud/centos-mycnf/master/my.cnf
-mv my.cnf /etc/my.cnf
+wget https://raw.githubusercontent.com/novuscloud/cybercp-mycnf/master/my.cnf
+mv my.cnf /etc/mysql/my.cnf
 cyberpanel createPackage --owner admin --packageName standard --diskSpace 10000 --bandwidth 0 --emailAccounts 1 --dataBases 1 --ftpAccounts 1 --allowedDomains 1
 cyberpanel createPackage --owner admin --packageName professional --diskSpace 15000 --bandwidth 0 --emailAccounts 10 --dataBases 10 --ftpAccounts 10 --allowedDomains 5
 cyberpanel createPackage --owner admin --packageName premium --diskSpace 25000 --bandwidth 0 --emailAccounts 0 --dataBases 0 --ftpAccounts 0 --allowedDomains 0
